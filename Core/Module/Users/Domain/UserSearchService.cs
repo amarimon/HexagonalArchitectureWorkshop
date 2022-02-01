188169a70cb508f1457e79018b91e476094504c0
amarimon <albert.marimon@gmail.com>
2022-02-01 23:39:30 +0100

    Create UserSearchService

---
+++ Core/Module/Users/Domain/UserSearchService.cs
@@ -0,0 +1,23 @@
+﻿using Core.Module.Shared.Domain;
+using Core.Module.Users.Domain;
+using System.Threading.Tasks;
+
+namespace Core.Module.Users.Domain
+{
+    public sealed class UserSearchService
+    {
+        private IUserRepository repository { get; set; }
+
+        public UserSearchService(IUserRepository userRepository)
+        {
+            this.repository = userRepository;
+        }
+
+        public async Task<User> Search(string userId)
+        {
+            User user = await this.repository.SearchAsyncById(new UserId(userId));
+
+            return user;
+        }
+    }
+}
